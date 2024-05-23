137c6e1c81514fcbe0ac2e338825a051ace6299a
sdiazm1 <69644320+sdiazm1@users.noreply.github.com>
2024-05-23 17:19:07 -0500

    Restaurant_start

--- app/src/main/java/com/example/fooduapp/ui/restaurant/RestaurantScreen.kt
+++ app/src/main/java/com/example/fooduapp/ui/restaurant/RestaurantScreen.kt
@@ -3,10 +3,14 @@ package com.example.fooduapp.ui.restaurant
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.fooduapp.R
 import com.example.fooduapp.ui.navigation.NavigationDestination
 
@@ -16,11 +20,26 @@ object RestaurantDestination : NavigationDestination {
 }
 @Composable
 fun RestaurantScreen(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Restaurant Screen")
-    }
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(
+                text = "Restaurants!",
+                fontSize = 32.sp,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Text(
+                text = "This are our restaurants available for you",
+                fontSize = 16.sp,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Text(
+                text = "Most popular",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
 }
